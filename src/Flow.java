2aee3c96b48e596f3e60e1f488e8307083f82433
leonan <leonan.marques886@gmail.com>
2023-01-02 19:03:00 -0300

    Feature:  repository have been created.

---
+++ src/Flow.java
@@ -0,0 +1,21 @@
+public class Flow {
+    public static void main(String[] args) {
+        System.out.println("Ini do main");
+        metodo1();
+        System.out.println("Fim do main");
+    }
+
+    private static void metodo1() {
+        System.out.println("Ini do metodo1");
+        metodo2();
+        System.out.println("Fim do metodo1");
+    }
+
+    private static void metodo2() {
+        System.out.println("Ini do metodo2");
+        for(int j = 1; j <= 5; j++) {
+            System.out.println(j);
+        }
+        System.out.println("Fim do metodo2");
+    }
+}
